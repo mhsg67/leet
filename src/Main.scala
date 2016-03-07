580a1950a61ed203f887f17b6eb6354f0161215b
mhsg67 <mhsedighi@gmail.com>
2016-03-07 02:55:40 -0500

    [master] completed the implementation

--- src/Main.scala
+++ src/Main.scala
@@ -1,10 +1,12 @@
 /**
- * User: tyson.hamilton
- * Date: 25/09/13
- * Time: 11:10 AM
- */
+  * User: tyson.hamilton
+  * Date: 25/09/13
+  * Time: 11:10 AM
+  */
 object Main extends App {
+
   object Leet {
+
     trait Leetable[T] {
       def toLeet(in: T): String
     }
@@ -13,4 +15,25 @@ object Main extends App {
   }
 
   // add your implementation below this line
+
+  /**
+    * Context bound for String to Leetable
+    * @return Leetable for String
+    */
+  implicit def strToLeetable = new Leet.Leetable[String] {
+    /**
+      * Mapping of char to numbers
+      * [a,A] -> 4
+      * [e,E] -> 3
+      * [i,I] -> 1
+      * [o,O] -> 0
+      * [s,S] -> 5
+      * [t,T] -> 7
+      */
+    private val leetMap = Map(('a' -> '4'), ('e' -> '3'), ('i' -> '1'), ('o' -> '0'), ('s' -> '5'), ('t' -> '7'))
+
+    override def toLeet(rawString: String) = rawString.map(char => leetMap.get(char.toLower).getOrElse(char))
+  }
+
+  println(Leet(scala.io.StdIn.readLine()))
 }
